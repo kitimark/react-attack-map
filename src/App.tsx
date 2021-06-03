8c803c58836a77b2361ad829be95c21ea80583a7
Wasutan Kitijerapat <markdog_@hotmail.com>
2021-06-03 15:43:53 +0700

    Implemented world map

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from 'd3';
+import * as d3Geo from 'd3-geo';
+import { GeoPermissibleObjects } from 'd3-geo';
+//@ts-ignore
+import * as d3GeoProjection from 'd3-geo-projection';
+import countryShapes from './geo.json';
+import { useChartDimensions } from './useChartDimension';
+
+const Map = ({ projectionName = "geoArmadillo" }) => {
+  // grab our custom React hook we defined above
+  const [ref, dms] = useChartDimensions({})
+
+  // this is the definition for the whole Earth
+  const sphere: GeoPermissibleObjects = { type: "Sphere" }
+
+  console.log(d3GeoProjection);
+  const projectionFunction: any = d3Geo[projectionName as keyof typeof d3Geo]
+    || d3GeoProjection[projectionName];
+  const projection = projectionFunction()
+    .fitWidth(dms.width, sphere)
+  const pathGenerator = d3.geoPath(projection)
+
+  // size the svg to fit the height of the map
+  const [
+    [x0, y0],
+    [x1, y1]
+  ] = pathGenerator.bounds(sphere)
+  const height = y1
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        width: "100%",
+      }}
+    >
+      <svg width={dms.width} height={height}>
+        <defs>
+{/* some projections bleed outside the edges of the Earth's sphere */}
+{/* let's create a clip path to keep things in bounds */}
+          <clipPath id="Map__sphere">
+            <path d={pathGenerator(sphere) || undefined} />
+          </clipPath>
+        </defs>
+
+        <path
+          d={pathGenerator(sphere) || undefined}
+          fill="#f2f2f7"
+        />
+
+        <g style={{ clipPath: "url(#Map__sphere)" }}>
+          {/* we can even have graticules! */}
+          <path
+            d={pathGenerator(d3.geoGraticule10()) || undefined}
+            fill="none"
+            stroke="#fff"
+          />
+
+          {countryShapes.features
+            .filter((shape) => shape.properties.iso_a3 !== 'ATA')
+            .map((shape) => {
+            return (
+              <path
+                key={shape.properties.subunit}
+                d={pathGenerator(shape as any) || undefined}
+                fill="#9980FA"
+                stroke="#fff"
+              >
+                <title>
+                  {shape.properties.name}
+                </title>
+              </path>
+            )
+          })}
+        </g>
+      </svg>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map projectionName="geoMiller" />
+    // <svg>
+    //   <circle cx="100" cy="100" r="50"></circle>
+    // </svg>
   );
 }
 
---
+++ src/useChartDimension.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from 'react'
+import { ResizeObserver } from '@juggle/resize-observer'
+
+const combineChartDimensions = (dimensions: any) => {
+  const parsedDimensions = {
+      ...dimensions,
+      marginTop: dimensions.marginTop || 10,
+      marginRight: dimensions.marginRight || 10,
+      marginBottom: dimensions.marginBottom || 40,
+      marginLeft: dimensions.marginLeft || 75,
+  }
+  return {
+      ...parsedDimensions,
+      boundedHeight: Math.max(
+        parsedDimensions.height
+        - parsedDimensions.marginTop
+        - parsedDimensions.marginBottom,
+        0,
+      ),
+      boundedWidth: Math.max(
+        parsedDimensions.width
+        - parsedDimensions.marginLeft
+        - parsedDimensions.marginRight,
+        0,
+      ),
+  }
+}
+
+export const useChartDimensions = (passedSettings: any) => {
+  const ref = useRef()
+  const dimensions = combineChartDimensions(
+    passedSettings
+  )
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+  useEffect(() => {
+      if (dimensions.width && dimensions.height)
+        return;
+      const element = ref.current
+      const resizeObserver = new ResizeObserver(
+        (entries: any) => {
+          if (!Array.isArray(entries)) return
+          if (!entries.length) return
+          const entry = entries[0]
+          if (width != entry.contentRect.width)
+            setWidth(entry.contentRect.width)
+          if (height != entry.contentRect.height)
+            setHeight(entry.contentRect.height)
+        }
+      )
+      resizeObserver.observe(element as any)
+      return () => resizeObserver.unobserve(element as any)
+  }, [])
+  const newSettings = combineChartDimensions({
+      ...dimensions,
+      width: dimensions.width || width,
+      height: dimensions.height || height,
+  })
+  return [ref, newSettings]
+}
